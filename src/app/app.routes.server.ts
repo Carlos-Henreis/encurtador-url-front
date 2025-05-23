6e795b1c9dd08739452f0415e9dc464bd09789d9
carlos-henreis <carlos_henreis@yahoo.com.br>
2025-05-23 20:09:25 -0300

    ajustes finais

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
+    path: '',
+    renderMode: RenderMode.Prerender 
+  },
+  {
+    path: 'stats/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: '**', // 👈 isso resolve o erro
+    renderMode: RenderMode.Server
   }
 ];
